f408762809f59dec296eca076f00def7f8a57873
Guilherme <guiialexandrino@gmail.com>
2023-01-16 00:10:16 -0300

    Melhora performance

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,11 @@ import Utils from './utils.js';
 
 /* Variaveis Globais */
 
-const __pokedexNumber = 351;
+const __pokedexNumber = 386;
+let __loadingProcess = 0;
 let __pokedex = [];
 let __pokedexBackup = [];
 let __selectedPoke = {};
-let __clickPosition = {};
 let __dialogInfo = false;
 
 /* Elementos Reativos da página */
@@ -63,42 +63,62 @@ window.addEventListener(
 /* Métodos iniciais para carregar pokes */
 
 async function getGeneralInfoPokes(id) {
-  return fetch(`https://pokeapi.co/api/v2/pokemon/${id}`).then((response) =>
-    response.json()
+  return fetch(`https://pokeapi.co/api/v2/pokemon/${id}`).then((response) => {
+    __loadingProcess++;
+
+    document.querySelector('#loading-status').innerHTML = `${Math.round(
+      (__loadingProcess / (__pokedexNumber * 2)) * 100
+    )} %`;
+
+    return response.json();
+  });
+}
+
+async function addExtraInfo(id) {
+  return fetch(`https://pokeapi.co/api/v2/pokemon-species/${id}`).then(
+    (response) => {
+      __loadingProcess++;
+
+      document.querySelector('#loading-status').innerHTML = `${Math.round(
+        (__loadingProcess / (__pokedexNumber * 2)) * 100
+      )} %`;
+
+      return response.json();
+    }
   );
 }
 
-async function addExtraInfo(pokedexArray, id) {
-  return fetch(`https://pokeapi.co/api/v2/pokemon-species/${id}`)
-    .then((response) => response.json())
-    .then((info) =>
-      Object.assign(pokedexArray[id - 1], {
-        genera: info.genera,
-        flavor_text_entries: info.flavor_text_entries,
-      })
-    );
-}
-
 async function getPokedex() {
+  const pokemonPromises = [];
+  const pokemonInfoPromises = [];
+
+  loading.style.display = 'flex';
   for (let i = 1; i <= __pokedexNumber; i++) {
-    loading.style.display = 'flex';
-    try {
-      __pokedex.push(await getGeneralInfoPokes(i));
-      await addExtraInfo(__pokedex, i);
-      document.querySelector('#loading-status').innerHTML = `${Math.round(
-        (i / __pokedexNumber) * 100
-      )} %`;
-    } catch (error) {
-      console.error(error);
-    }
+    pokemonPromises.push(getGeneralInfoPokes(i));
   }
+
+  await Promise.all(pokemonPromises).then((pokes) => {
+    __pokedex = pokes;
+  });
+
+  for (let i = 1; i <= __pokedexNumber; i++) {
+    pokemonInfoPromises.push(addExtraInfo(i));
+  }
+
+  await Promise.all(pokemonInfoPromises).then((infoPokes) => {
+    infoPokes.forEach((poke, index) => {
+      Object.assign(__pokedex[index], {
+        genera: poke.genera,
+        flavor_text_entries: poke.flavor_text_entries,
+      });
+    });
+  });
 
   for (let i in __pokedex) {
     __pokedex[i].name =
       __pokedex[i].name[0].toUpperCase() + __pokedex[i].name.substring(1);
     __pokedex[i].idNumber = Number(i) + 1;
   }
-
   createInterface();
 }
 
